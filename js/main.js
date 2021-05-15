6ab55e2f181e76cc9d9116ec80937550e7a53e12
aaalyo <67644184+aaalyo@users.noreply.github.com>
2021-05-15 12:03:32 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const myFullpage = new fullpage('#fullpage', {
+    
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    menu: '#menu',
+    lazyLoad: true
+});
+
+
+
+function myMap() {
+    const mapProp = {
+        center: new google.maps.LatLng(51.33765, 0.60302),
+        zoom: 15,
+    };
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    const marker = new google.maps.Marker({
+        position: {lat:51.33765, lng:0.60302},
+        map: map,
+      });
+}
