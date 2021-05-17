fcae13ab43c35218a6c1647ecf35aa7ec5cbda68
aaalyo <67644184+aaalyo@users.noreply.github.com>
2021-05-17 16:14:12 +0300

    login page

--- singin.js
+++ singin.js
@@ -1,24 +1,58 @@
 
-const registrationForm = document.getElementById('register-form')
-const submitButton = registrationForm.elements['submit']
-const passwordInput =registrationForm.elements['password']
+// const registrationForm = document.getElementById('register-form')
+// const submitButton = registrationForm.elements['submit']
+// const passwordInput =registrationForm.elements['password']
 
 
-registrationForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+// registrationForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
 
-    if (passwordInput.value !== usernameInput.value) {
-        alert('Error')
-        return
-    }
+//     if (passwordInput.value !== usernameInput.value) {
+//         alert('Error')
+//         return
+//     }
 
-    alert('Successfully submitted')
-})
+//     alert('Successfully submitted')
+// })
 
-registrationForm.elements['TandC'].addEventListener('change', (event) => {
-    if (event.target.checked) {
-        submitButton.disabled = false
-    } else {
-        submitButton.disabled = true
-    }
-})
+// registrationForm.elements['TandC'].addEventListener('change', (event) => {
+//     if (event.target.checked) {
+//         submitButton.disabled = false
+//     } else {
+//         submitButton.disabled = true
+//     }
+// })
+
+$(function () {
+    $("#datepicker-from").datepicker();
+});
+
+$(function () {
+    $("#datepicker-till").datepicker();
+});
+
+
+
+// $('.sign-up-link').click(function(){
+// $('.sign-in').hide( "slow")
+
+// }
+// );
+
+$(document).ready(function () {
+    $("#hide").click(function () {
+        $(".sign-in").hide();
+    });
+    $("#hide").click(function () {
+        $(".register").show();
+    });
+
+    $("#hide").click(function () {
+        $(".card-footer").hide();
+    });
+
+    $("#hide").click(function () {
+        $(".card-header-text").html("Register");
+    });
+
+});
